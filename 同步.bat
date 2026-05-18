132f32c4010f9f41c80c92756c1b5fb692448661
daishizhe <daishizhe@corp.netease.com>
2026-05-18 16:40:53 +0800

    周一 2026/05/18 16:40:49

--- 同步.bat
+++ 同步.bat
@@ -1,12 +1,14 @@
 @echo off
 chcp 65001 >nul
+cd /d "%~dp0"
 set CURRENT_TIME=%date% %time:~0,8%
+echo %CURRENT_TIME%
 echo 拉取最新代码
 git pull origin main
 echo 添加文件到暂存区
 git add .
 echo 正在提交代码
-git commit -m "%日期+时间%"
+git commit -m "%CURRENT_TIME%"
 echo 推送到远程仓库
 git push origin main
 pause
